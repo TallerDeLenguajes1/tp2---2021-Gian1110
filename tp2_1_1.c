4996bc88c5b05bf0fef91474f9af06cf486cd3da
gian1110 <gianlucaberarducci@gmail.com>
2021-04-14 19:27:44 -0300

    correcciones y cargado de numeros para ver mejor la matriz

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -3,14 +3,23 @@
 #define M 5
 
 int main(){
-    int f,c,d;
+    int f,c,d=1;
     double mt [N][M];
+
+     for ( f = 0; f < N; f++)
+    {
+        for (c = 0; c < M; c++)
+        {
+             mt[f][c]=d;   
+             d++;
+        }
+    }
+
     for ( f = 0; f < N; f++)
     {
-        for (f = 0; f < N; f++)
+        for (c = 0; c < M; c++)
         {
-             printf("%1f",mt[f][c]);   
-             
+             printf("%.1f ",mt[f][c]);            
         }
         printf("\n");
     }
