785dbbee6af18087d0859d5ed17adff6bcb1ba00
gian1110 <gianlucaberarducci@gmail.com>
2021-04-14 17:58:36 -0300

    se creo el archivo

---
+++ tp2_1_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define N 4
+#define M 5
+
+int main(){
+    int f,c,d;
+    double mt [N][M];
+    for ( f = 0; f < N; f++)
+    {
+        for (f = 0; f < N; f++)
+        {
+             printf("%1f",mt[f][c]);   
+             
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
